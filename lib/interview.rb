ef2a2d4a5e24b7ca7456be02bbd0ada07bfcdef3
Daniel Bogan <d+github@waferbaby.com>
2012-04-29 19:44:53 -0700

    Initial import.

---
+++ lib/interview.rb
@@ -0,0 +1,21 @@
+$:.unshift File.dirname(__FILE__)
+
+require 'mongoon/document'
+
+class Interview < Mongoon::Document
+        attr_accessor :name, :slug, :date, :contents, :credits
+        
+        def self.find_recent
+                self.find({}, {:limit => 10}) # Sort by date here.
+        end
+        
+        def self.find_by_slug(slug)
+                self.find({'slug' => slug})[0]
+        end
+        
+        def self.find_by_category(category)
+        end
+        
+        def self.find_by_date(year, month = nil, day = nil)
+        end
+end
