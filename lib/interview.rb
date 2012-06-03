8eca031bc59dd3dbab01a0759172bd04ed0fa61b
Daniel Bogan <d+github@waferbaby.com>
2012-06-03 14:13:48 -0700

    Added the missing fields, fleshed out self.recent to take options.

--- lib/interview.rb
+++ lib/interview.rb
@@ -4,7 +4,7 @@ require 'resource'
 
 class Interview < Resource
         
-        attr_accessor :slug, :name, :summary, :overview, :hardware, :software, :dream_setup, :date_create
+        attr_accessor :slug, :name, :summary, :credit_name, :credit_url, :overview, :hardware, :software, :dream_setup, :published_on, :date_create, :date_update
         attr_accessor :categories, :wares
         
         def initialize()
@@ -12,12 +12,15 @@ class Interview < Resource
                 wares = []
         end
         
-        def self.recent()
-                interviews = self.fetch("SELECT * FROM interviews ORDER BY date_create DESC")
+        def self.recent(options)
+                options = {:summary => false, :limit => 10}.merge!(options)
+                fields = options[:summary] ? "slug, name, summary, published_on" : "*"
+                
+                interviews = self.fetch("SELECT #{fields} FROM interviews ORDER BY published_on DESC LIMIT #{options[:limit]}")                
                 
                 interviews.each do |interview|
-                        categories = Categories.for_interview(interview.slug)
-                        wares = Wares.for_interview(interview.slug)
+                        #categories = Categories.for_interview(interview.slug)
+                        #wares = Wares.for_interview(interview.slug)
                 end
                 
                 interviews
