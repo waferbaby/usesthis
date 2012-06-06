c38aeb6c6ec3bf8d97d69a53a8785e07a8e14bad
Daniel Bogan <d+github@waferbaby.com>
2012-06-06 00:57:07 -0700

    Added an id field, actual categories, and all the wares if we're not requesting the summary.

--- lib/interview.rb
+++ lib/interview.rb
@@ -1,10 +1,11 @@
 $:.unshift File.dirname(__FILE__)
 
-require 'resource'
+require 'category'
+require 'ware'
 
 class Interview < Resource
         
-        attr_accessor :slug, :name, :summary, :credit_name, :credit_url, :overview, :hardware, :software, :dream_setup, :published_on, :date_create, :date_update
+        attr_accessor :id, :slug, :name, :summary, :credit_name, :credit_url, :overview, :hardware, :software, :dream_setup, :published_on, :date_create, :date_update
         attr_accessor :categories, :wares
         
         def initialize()
@@ -14,13 +15,13 @@ class Interview < Resource
         
         def self.recent(options)
                 options = {:summary => false, :limit => 10}.merge!(options)
-                fields = options[:summary] ? "slug, name, summary, published_on" : "*"
+                fields = options[:summary] ? "id, slug, name, summary, published_on" : "*"
                 
                 interviews = self.fetch("SELECT #{fields} FROM interviews ORDER BY published_on DESC LIMIT #{options[:limit]}")                
                 
                 interviews.each do |interview|
-                        #categories = Categories.for_interview(interview.slug)
-                        #wares = Wares.for_interview(interview.slug)
+                        interview.categories = Category.for_interview(interview.id)
+                        interview.wares = Ware.for_interview(interview.id) unless options[:summary]
                 end
                 
                 interviews
