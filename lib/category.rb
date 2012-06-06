a0c14859f8359c9e846f2c8e75b8c4099f87c0e3
Daniel Bogan <d+github@waferbaby.com>
2012-06-06 11:06:28 -0700

    Specified what table to return bits from.

--- lib/category.rb
+++ lib/category.rb
@@ -7,6 +7,6 @@ class Category < Resource
         attr_accessor :id, :slug
         
         def self.for_interview(id)
-                self.fetch("SELECT id, slug FROM categories AS c, interview_categories AS i WHERE c.id=i.category_id AND i.interview_id=#{id}")
+                self.fetch("SELECT c.id, c.slug FROM categories AS c, interview_categories AS i WHERE c.id=i.category_id AND i.interview_id=#{id}")
         end
 end
