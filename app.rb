8fd06ae4c91d20e7883eece262069e50e798fd77
Daniel Bogan <d+github@waferbaby.com>
2012-06-07 23:09:45 -0700

    Added support for category and year archive views, and added titles everywhere.

--- app.rb
+++ app.rb
@@ -9,9 +9,10 @@ class TheSetup < Sinatra::Base
         configure do
                 begin
                         config = YAML::load_file(File.join(Dir.pwd, 'config.yml'))
-                
+
                         Resource.database = Mysql2::Client.new(config[:database])                
                         Slim::Engine.set_default_options(:pretty => true)
+                        
                 rescue Exception => e
                         puts e
                 end
@@ -23,9 +24,32 @@ class TheSetup < Sinatra::Base
         end
         
         get '/interviews/?' do
+                @title = "Interviews"
+                
+                @stats = Interview.counts()
+                @categories = Category.all()
+                
+                slim :interviews
+        end
+        
+        get %r{/interviews/([\d]{4})/?} do |year|
+                
+                @interviews = Interview.by_year(year, :summary => true)
+                @title = "In #{year}" if @interviews.count
+                
+                slim :index
+        end
+        
+        get %r{/interviews/([a-z]+)/?} do |slug|
+                @interviews = Interview.for_category_slug(slug, :summary => true)
+                @title = slug.capitalize if @interviews.count
+                
+                slim :index
         end
         
         get '/about/?' do
+                @title = "About"
+                
                 slim :about
         end
         
