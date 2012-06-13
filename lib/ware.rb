9ee6e232fb13b5a3207960c6baebeb4a86b161b8
Daniel Bogan <d@waferbaby.com>
2012-06-13 16:05:15 -0700

    Updated the query in self.for_interview to order by the slug.

--- lib/ware.rb
+++ lib/ware.rb
@@ -7,6 +7,6 @@ class Ware < Resource
         attr_accessor :id, :slug, :type, :name, :description, :url, :date_create, :date_update
         
         def self.for_interview(id)
-                self.fetch("SELECT w.* FROM wares AS w, interview_wares AS i WHERE w.id=i.ware_id AND i.interview_id=#{id}")
+                self.fetch("SELECT w.* FROM wares AS w, interview_wares AS i WHERE w.id=i.ware_id AND i.interview_id=#{id} ORDER BY w.slug")
         end
 end
