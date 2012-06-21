15e2424456728f7e5443b11bf6a8db9bb9f36c58
Daniel Bogan <d@waferbaby.com>
2012-06-20 22:13:56 -0700

    Ordered categories by slug for the self.for_interview method.

--- lib/category.rb
+++ lib/category.rb
@@ -7,7 +7,7 @@ class Category < Resource
         attr_accessor :id, :slug
         
         def self.for_interview(id)
-                self.fetch("SELECT c.id, c.slug FROM categories AS c, interview_categories AS i WHERE c.id=i.category_id AND i.interview_id=#{id}")
+                self.fetch("SELECT c.id, c.slug FROM categories AS c, interview_categories AS i WHERE c.id=i.category_id AND i.interview_id=#{id} ORDER BY c.slug")
         end
         
         def self.all
