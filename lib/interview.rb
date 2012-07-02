3cbb63ecdabfe76a871e2933e5f8657f1e8c3cbf
Daniel Bogan <d@waferbaby.com>
2012-07-01 21:48:56 -0700

    Check the is_published flag instead of the date.

--- lib/interview.rb
+++ lib/interview.rb
@@ -33,7 +33,7 @@ class Interview < Resource
                 
                 fields = options[:summary] ? "id, slug, name, summary, published_on" : "*"
                 
-                self.fetch("SELECT #{fields} FROM interviews WHERE published_on > 0", options)                
+                self.fetch("SELECT #{fields} FROM interviews WHERE is_published=1", options)
         end
         
         def self.with_slug(slug, options = {})
@@ -49,7 +49,7 @@ class Interview < Resource
                 options = {:summary => false, :order_by => 'published_on DESC'}.merge!(options)
                 fields = options[:summary] ? "id, slug, name, summary, published_on" : "*"
                 
-                self.fetch("SELECT * FROM interviews WHERE year(published_on) = '#{year}' AND published_on > 0", options)
+                self.fetch("SELECT * FROM interviews WHERE year(published_on) = '#{year}' AND is_published=1", options)
         end
         
         def self.for_category_slug(slug, options = {})
@@ -58,11 +58,11 @@ class Interview < Resource
                 options = {:summary => false, :order_by => 'i.published_on DESC'}.merge!(options)
                 fields = options[:summary] ? "i.id, i.slug, i.name, i.summary, i.published_on" : "i.*"
                 
-                self.fetch("SELECT #{fields} FROM interviews AS i, interview_categories AS ic, categories AS c WHERE ic.interview_id=i.id AND ic.category_id=c.id AND c.slug = '#{slug}' AND i.published_on > 0", options)
+                self.fetch("SELECT #{fields} FROM interviews AS i, interview_categories AS ic, categories AS c WHERE ic.interview_id=i.id AND ic.category_id=c.id AND c.slug = '#{slug}' AND i.is_published=1", options)
         end
 
         def self.counts()
-                Interview.query("SELECT year(published_on) AS year, count(*) AS count FROM interviews WHERE published_on > 0 GROUP BY year ORDER BY year DESC")
+                Interview.query("SELECT year(published_on) AS year, count(*) AS count FROM interviews WHERE is_published=1 GROUP BY year ORDER BY year DESC")
         end
         
         def to_markdown
