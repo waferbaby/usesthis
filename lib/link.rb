b16bf8ed0c5e432b129b1ced2ea540a934202f09
Daniel Bogan <d@waferbaby.com>
2012-07-28 13:17:09 -0700

    Added self.inspired_by and self.personal.

--- lib/link.rb
+++ lib/link.rb
@@ -4,9 +4,20 @@ require 'resource'
 
 class Link < Resource
         
+        INSPIRED = 1
+        PERSONAL = 2
+        
         attr_accessor :id, :type, :name, :url, :description
         
         def self.all
                 self.fetch("SELECT * from links ORDER BY name")
+        end
+        
+        def self.inspired_by
+                self.fetch("SELECT * from links WHERE type=#{INSPIRED} ORDER BY name")
+        end
+        
+        def self.personal
+                self.fetch("SELECT * from links WHERE type=#{PERSONAL} ORDER BY name")
         end
 end
