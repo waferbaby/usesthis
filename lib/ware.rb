22d7368a5c1c851c84270f871bc84fde663134cd
Daniel Bogan <d@waferbaby.com>
2013-01-06 00:29:03 -0800

    Cleanup, set the table_name.

--- lib/ware.rb
+++ lib/ware.rb
@@ -8,17 +8,22 @@ class Ware < Resource
         SOFTWARE = 2
         
         attr_accessor :id, :slug, :type, :name, :description, :url, :date_create, :date_update
+        @table_name = 'wares'
         
         def self.for_interview(id)
+
                 self.fetch("SELECT w.* FROM wares AS w, interview_wares AS i WHERE w.id=i.ware_id AND i.interview_id=#{id} ORDER BY w.slug")
-	rescue Exception => e
-		raise WareException.new("Failed to fetch wares for interview #{id} (#{e})")
+	
+        rescue Exception => e
+	      raise WareException.new("Failed to fetch wares for interview #{id} (#{e})")
         end
         
         def self.with_slug(slug, options = {})
+
                 result = self.fetch("SELECT * FROM wares WHERE slug='#{self.escape(slug)}'", options)                
                 result.length < 1 ? nil : result[0]
-	rescue Exception => e
+	
+        rescue Exception => e
 		raise WareException.new("Failed to fetch ware with slug '#{slug}' (#{e})")
         end
         
@@ -32,7 +37,8 @@ class Ware < Resource
         
         def self.popular(type, options = {})
                 self.fetch("SELECT w.*, COUNT(w.id) as weight FROM wares AS w, interview_wares AS iw WHERE w.type=#{type} AND w.id=iw.ware_id GROUP BY w.id ORDER BY weight DESC LIMIT 10", options)
-	rescue Exception => e
+	
+        rescue Exception => e
 		raise WareException.new("Failed to fetch popular wares (#{e})")
         end
 end
