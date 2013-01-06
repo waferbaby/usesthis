95bf759d201778619aa19a9e78b8f3cef4a36079
Daniel Bogan <d@waferbaby.com>
2013-01-06 00:34:29 -0800

    Set the table_name, use the insert function.

--- lib/category.rb
+++ lib/category.rb
@@ -5,8 +5,10 @@ require 'resource'
 class Category < Resource
         
         attr_accessor :id, :slug, :date_create
+        @table_name = 'categories'
         
 	def self.with_slug(slug, create_if_new = false)
+	
 		result = self.fetch("SELECT * FROM categories WHERE slug='#{self.escape(slug)}' LIMIT 1")
 		
 		return result[0] if result.length == 1
@@ -18,31 +20,36 @@ class Category < Resource
 		category.save
 			
 		category
+	
 	rescue Exception => e
 		raise CategoryException.new("Failed to fetch category with slug '#{slug}' (#{e})")
 	end
 	
         def self.for_interview(id)
+        
                 self.fetch("SELECT c.id, c.slug FROM categories AS c, interview_categories AS i WHERE c.id=i.category_id AND i.interview_id=#{id} ORDER BY c.slug")
+	
 	rescue Exception => e
 		raise CategoryException.new("Failed to fetch categories for interview #{id} (#{e})")
         end
         
         def self.all
+        
                 self.fetch("SELECT id, slug from categories ORDER BY slug")
+	
 	rescue Exception => e
 		raise CategoryException.new("Failed to fetch all categories (#{e})")
         end
-	
+
         def save
+	
 		now = Time.now
 		time = now.strftime('%Y-%m-%d %H:%M:%S')
 		
-                Category.query("INSERT INTO categories (slug, date_create) VALUES ('#{Category.escape(@slug)}', '#{Category.escape(time)}')")
-		
-		@id = Category.last_insert_id		
+		@id = Category.insert({'slug' => @slug, 'date_create' => time})
 		@date_create = now
-	rescue
+	
+	rescue Exception => e
 		raise CategoryException.new(e.to_s =~ /Duplicate entry/ ? "A category with this slug already exists" : "Failed to save new category (#{e})")
         end
 end
