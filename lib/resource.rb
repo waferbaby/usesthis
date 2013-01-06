ecf02a7a4ecee4e9e9d122adef3b412b2adec16a
Daniel Bogan <d@waferbaby.com>
2013-01-06 00:28:47 -0800

    Added the table_name class variable, and the insert and update class methods.

--- lib/resource.rb
+++ lib/resource.rb
@@ -3,10 +3,11 @@ require 'mysql2'
 
 class Resource
         class << self
-                attr_accessor :database
+                attr_accessor :database, :table_name
         end
                 
         def self.fetch(query, options = {})
+                
                 items = []
                 
                 Resource.database.query(query).each do |row|
@@ -17,16 +18,40 @@ class Resource
                 
                 items
         end
-        
+
         def self.query(query)
                 Resource.database.query(query)
         end
 	
 	def self.last_insert_id
-		Resource.database.last_id
+                Resource.database.last_id
 	end
         
         def self.escape(string)
                 Resource.database.escape(string.to_s)
+        end
+
+        def self.insert(fields)
+
+                keys = fields.keys
+                values = fields.values
+                
+                query = "INSERT INTO #{@table_name} ("
+                query += keys.join(', ')
+                query += ") VALUES ("
+                query += values.map {|value| "'#{Resource.escape(value)}'"}.join(", ")
+                query += ")"
+                
+                Resource.query(query)
+                Resource.last_insert_id
+        end
+
+        def self.update(id, fields)
+
+                query = "UPDATE #{@table_name} SET "
+                query += fields.map {|key, value| "#{key}='#{Resource.escape(value)}'" }.join(", ")
+                query += " WHERE id=#{id}"
+
+                Resource.query(query)
         end
 end
