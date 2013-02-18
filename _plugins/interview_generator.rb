a923df6a7f23bab2970e90375fc248b43be029b3
Daniel Bogan <d@waferbaby.com>
2013-02-17 17:00:54 -0800

    Preload all the wares, once, to speed things up.

--- _plugins/interview_generator.rb
+++ _plugins/interview_generator.rb
@@ -13,21 +13,26 @@ module Jekyll
 
 		def convert(content)
 
-			wares_path = File.join(@config['source'], 'wares')
+			if @wares.nil?
+				@wares = {}
+
+				Dir.glob(File.join(@config['source'], 'wares', "*.yml")) do |path|
+					begin
+						@wares[File.basename(path, File.extname(path))] = YAML::load_file(path)
+					rescue
+					end
+				end
+			end
 
 			content += "\n\n"
 
 			content.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/).each do |link|
 				slug = (link[1] ? link[1] : link[0].downcase)
-				path = File.join(wares_path, "#{slug}.yml")
 
-				if File.exists?(path)
-					begin
-						data = YAML::load_file(path)
-						content += "[#{slug}]: #{data['url']} \"#{data['description']}\"\n"
-					rescue
-					end
+				if data = @wares[slug]
+					content += "[#{slug}]: #{data['url']} \"#{data['description']}\"\n"
 				end
+				
 			end
 
 			Jekyll::MarkdownConverter.new(@config).convert(content.strip)
