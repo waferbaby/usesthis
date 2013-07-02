6a9f4776178019f88a587f52e5dd3d358d4a91ea
Daniel Bogan <d@waferbaby.com>
2013-07-02 14:59:07 -0700

    Switched to 2 soft tabs..

--- _plugins/community_page_generator.rb
+++ _plugins/community_page_generator.rb
@@ -1,44 +1,44 @@
 module Jekyll
 
-	class CommunityPage < Page
+  class CommunityPage < Page
 
-		def initialize(site, base, dir, links)
+    def initialize(site, base, dir, links)
 
-			@site = site
-			@base = base
-			@dir = dir
-			@name = 'index.html'
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
 
-			self.process(@name)
-			self.read_yaml(File.join(base, '_layouts'), 'community.html')
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'community.html')
 
-			self.data['links'] = links
-		end
-	end
+      self.data['links'] = links
+    end
+  end
 
-	class CommunityPageGenerator < Generator
+  class CommunityPageGenerator < Generator
 
-		safe true
+    safe true
 
-		def generate(site)
-			links = {
-				'inspired' => [],
-				'personal' => [],
-			}
-			
-			Dir.glob(File.join(site.source, "links", "**", "*.yml")) do |file|
+    def generate(site)
+      links = {
+        'inspired' => [],
+        'personal' => [],
+      }
+      
+      Dir.glob(File.join(site.source, "links", "**", "*.yml")) do |file|
 
-				data = YAML::load_file(file)
+        data = YAML::load_file(file)
 
-				case File.dirname(file).split(File::SEPARATOR)[-1]
-					when 'inspired'
-						links['inspired'] << data
-					when 'personal'
-						links['personal'] << data
-				end
-			end
+        case File.dirname(file).split(File::SEPARATOR)[-1]
+          when 'inspired'
+            links['inspired'] << data
+          when 'personal'
+            links['personal'] << data
+        end
+      end
 
-			site.pages << CommunityPage.new(site, site.source, 'community', links)
-		end
-	end
+      site.pages << CommunityPage.new(site, site.source, 'community', links)
+    end
+  end
 end
