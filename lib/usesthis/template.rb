2541162196db0718af2c1cdab0fb1f2f63d1f2fd
Daniel Bogan <d@waferbaby.com>
2013-07-07 23:15:31 -0700

    Wrapped in the UsesThis module. Grab the parent at render time.

--- lib/usesthis/template.rb
+++ lib/usesthis/template.rb
@@ -1,27 +1,30 @@
 require 'rubygems'
+require 'frontable'
 require 'tilt'
 
-class Template
-  attr_accessor :slug, :renderer, :parent
+module UsesThis
+  class Template
+    include Frontable
+    attr_accessor :site, :slug, :renderer, :metadata
 
-  def initialize(path, parent = nil)
-    @renderer = Tilt.new(path)
-    @slug = File.basename(path, File.extname(path))
+    def initialize(site, path)
+      @site = site
+      @renderer = Tilt.new(path)
+      @slug = File.basename(path, File.extname(path))
 
-    @parent = parent unless parent.nil?
-  end
-
-  def render(context, params, contents)
-    output = @renderer.render(context, params) {
-      contents
-    }
-    
-    if parent
-      output = parent.render(context, params, output) {
-        output
-      }
+      @metadata, _ = read_with_yaml(path)
     end
 
-    output
+    def render(context, params, contents)
+      output = @renderer.render(context, params) {
+        contents
+      }
+
+      if @metadata['layout'] && @site.templates[@metadata['layout']]
+        output = @site.templates[@metadata['layout']].render(context, params, contents)
+      end
+
+      output
+    end
   end
 end
