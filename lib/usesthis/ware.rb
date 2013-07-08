e18f51b37515d81f899ee92fd87a4b22ccc7ec94
Daniel Bogan <d@waferbaby.com>
2013-07-07 23:15:53 -0700

    Wrapped in the UsesThis module.

--- lib/usesthis/ware.rb
+++ lib/usesthis/ware.rb
@@ -1,16 +1,18 @@
 require 'rubygems'
 require 'yaml'
 
-class Ware
-  attr_accessor :slug, :name, :description, :url
+module UsesThis
+  class Ware
+    attr_accessor :slug, :name, :description, :url
 
-  def initialize(path)
-    metadata = YAML::load_file(path)
+    def initialize(path)
+      metadata = YAML::load_file(path)
 
-    @slug = File.basename(path, File.extname(path))
-    
-    %w(name description url).each do |item|
-      self.send("#{item}=", metadata[item])
+      @slug = File.basename(path, File.extname(path))
+      
+      %w(name description url).each do |item|
+        self.send("#{item}=", metadata[item])
+      end
     end
   end
 end
