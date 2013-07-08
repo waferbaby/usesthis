00bd67a47c7e2c1f16055bffa8bc05a6eca38027
Daniel Bogan <d@waferbaby.com>
2013-07-07 23:12:31 -0700

    Initial import.

---
+++ lib/usesthis/frontable.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'yaml'
+
+module Frontable
+  def read_with_yaml(path)
+    contents = File.read(path)
+      
+    if parts = contents.match(/^(-{3}\n.*?\n?)^(-{3}*$\n?)/m)
+      metadata = YAML::load(parts[1])
+      contents = parts.post_match.strip!
+    else
+      metadata = {}
+    end
+
+    [metadata, contents]
+  end
+end
