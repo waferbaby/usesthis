b90802ecbc5273c05960b48a15c0f9cb100f12bf
Daniel Bogan <d@waferbaby.com>
2013-07-07 19:28:31 -0700

    Load in templates, prepare their parents, make the build method actually do some stuff.

--- lib/usesthis/site.rb
+++ lib/usesthis/site.rb
@@ -1,13 +1,14 @@
 $:.unshift(__dir__)
 
 require 'rubygems'
+require 'fileutils'
 require 'interview'
 require 'link'
 require 'ware'
 
 class Site
   
-  attr_accessor :paths, :interviews, :wares, :links, :categories
+  attr_accessor :paths, :interviews, :wares, :links, :templates, :categories
 
   def initialize(path)
     @paths = {
@@ -15,15 +16,27 @@ class Site
       output: File.join(path, 'site'),
       data: File.join(path, 'data'),
       assets: File.join(path, 'public'),
+      templates: File.join(path, 'templates')
     }
 
     self.scan
   end
 
   def scan
+    @templates = {}
+
+    Dir.glob(File.join(@paths[:source], 'templates', '*.*')).each do |path|
+      template = Template.new(path)
+      @templates[template.slug] = template
+    end
+
+    @templates.each do |key, value|
+      @templates[key].parent = @templates['default'] if key != 'default'
+    end
+
     @wares = {}
 
-    Dir.glob(File.join(@paths[:data], '*', '*.*')).each do |path|
+    Dir.glob(File.join(@paths[:data], 'wares', '**', '*.*')).each do |path|
       ware = Ware.new(path)
       @wares[ware.slug] = ware
     end
@@ -35,10 +48,12 @@ class Site
 
       interview.answers.scan(/\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/).each do |link|
         slug = (link[1] ? link[1] : link[0].downcase)
-        interview.wares << @wares[slug] if @wares[slug]
+        interview.wares.push(@wares[slug]) if @wares[slug]
       end
 
-      @interviews << interview
+      interview.template = @templates['interview']
+
+      @interviews.push(interview)
     end
 
     @interviews.reverse!
@@ -47,11 +62,20 @@ class Site
     
     interviews.each do |interview|
       interview.categories.each do |category|
-        (@categories[category] ||= []) << interview
+        (@categories[category] ||= []).push(interview)
       end
     end
   end
 
   def build
+    if Dir.exists?(@paths[:output])
+      FileUtils.rm_rf(Dir.glob(File.join(@paths[:output], '*')))
+    else
+      Dir.mkdir(@paths[:output])
+    end
+
+    @interviews.each do |interview|
+      interview.write(File.join(@paths[:output], 'interviews'))
+    end
   end
 end
