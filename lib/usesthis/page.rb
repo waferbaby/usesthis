d7f62aa55bf6350ddeed641b62ce6efa80222a7b
Daniel Bogan <d@waferbaby.com>
2013-07-10 00:32:39 -0700

    I *think* this makes sense to do.

--- lib/usesthis/page.rb
+++ lib/usesthis/page.rb
@@ -1,24 +1,30 @@
 $:.unshift(__dir__)
 
 require 'rubygems'
+require 'template'
+require 'erubis'
 
 module UsesThis
-  class Page
+  class Page < Template
     include Frontable
-    attr_accessor :site, :metadata, :contents
-
-    def initialize(site, path = nil)
-      @site = site
-      @metadata, @contents = path ? read_with_yaml(path) : {}, ''
-    end
 
     def write(output_path)
-      path = File.join(output_path, 'index.html')
+      if @path
+        path = File.join(output_path, File.dirname(@path).gsub(@site.paths[:pages], ''), 'index.html')
+      else
+        path = File.join(output_path, 'index.html')
+      end
 
       FileUtils.mkdir_p(File.dirname(path))
-      
+
       File.open(path, 'w') do |file|
-        file.write(@site.templates[@metadata[:layout]].render(@metadata, @contents))
+        begin
+          output = self.render(@metadata, @contents)
+        rescue
+          output = @contents
+        end
+
+        file.write(output)
       end
     end
   end
