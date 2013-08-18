e3178fce071efde9f5fcfa010ffdac77087d426d
Daniel Bogan <d@waferbaby.com>
2013-08-17 19:24:07 -0700

    Look in data/wares again.

--- lib/usesthis/site.rb
+++ lib/usesthis/site.rb
@@ -32,7 +32,7 @@ module UsesThis
 
       @wares = {}
 
-      Dir.glob(File.join(@paths[:data], '{hardware,software}', '*.yml')).each do |path|
+      Dir.glob(File.join(@paths[:data], 'wares', '**', '*.yml')).each do |path|
         ware = Ware.new(path)
         @wares[ware.slug] = ware
       end
