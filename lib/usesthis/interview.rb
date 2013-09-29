17712aa0223e3c84411e40bf18ca6f3227c59c37
Daniel Bogan <d@waferbaby.com>
2013-09-29 13:55:11 -0700

    Define the layout for interviews so it's not needed in the YAML.

--- lib/usesthis/interview.rb
+++ lib/usesthis/interview.rb
@@ -1,9 +1,13 @@
 module UsesThis
   class Interview < Salt::Post
 
+    def initialize(site, path)
+      super(site, path)
+      @metadata['layout'] = 'interview'
+    end
+
     def output_path(parent_path)
       File.join(parent_path, 'interviews', @slug)
     end
-
   end
 end
