7c3e2e2a63f5eba0ba6e067abb6869bc0ec8be92
Daniel Bogan <d@waferbaby.com>
2013-10-05 19:14:58 -0700

    Load the link, ware and site classes.

--- lib/usesthis.rb
+++ lib/usesthis.rb
@@ -3,5 +3,6 @@ $:.unshift(__dir__)
 require 'salt'
 
 require 'usesthis/interview'
-#require 'usesthis/link'
-#require 'usesthis/ware'
+require 'usesthis/link'
+require 'usesthis/ware'
+require 'usesthis/site'
