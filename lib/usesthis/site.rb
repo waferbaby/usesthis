21dcd5c39a7aba6015597492a5078f7cac55d9f5
Daniel Bogan <d@waferbaby.com>
2013-10-05 22:07:45 -0700

    Added overrides for setup and scan_files, to pull in wares and links.

--- lib/usesthis/site.rb
+++ lib/usesthis/site.rb
@@ -1,4 +1,33 @@
 module UsesThis
   class Site < Salt::Site
+
+    attr_accessor :wares, :links
+
+    def setup(path = nil, config = {})
+      super
+
+      @paths[:wares] = File.join(@paths[:source], 'data', 'wares')
+      @paths[:links] = File.join(@paths[:source], 'data', 'links')
+
+      @wares, @links = {}, { personal: [], inspired: [] }
+    end
+
+    def scan_files
+      super
+
+      Dir.glob(File.join(@paths[:wares], '**', '*.yml')).each do |path|
+        ware = UsesThis::Ware.new(path)
+        @wares[ware.slug] = ware
+      end
+
+      Dir.glob(File.join(@paths[:links], '**', '*.yml')).each do |path|
+        link = UsesThis::Link.new(path)
+        @links[link.summary ? :inspired : :personal] << link
+      end
+
+      @posts.each do |post|
+        post.scan_links
+      end
+    end
   end
 end
