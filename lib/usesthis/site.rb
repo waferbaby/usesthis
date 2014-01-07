05ba92c797311ccf8148fda8a7c2003f0159d995
Daniel Bogan <d@waferbaby.com>
2014-01-06 21:16:20 -0800

    Updated to configure all the new site settings, and matched the new path variables.

--- lib/usesthis/site.rb
+++ lib/usesthis/site.rb
@@ -4,25 +4,33 @@ module UsesThis
     attr_accessor :wares, :links
 
     def setup(path = nil, config = {})
-      super
-
-      @paths[:wares] = File.join(@paths[:source], 'data', 'wares')
-      @paths[:links] = File.join(@paths[:source], 'data', 'links')
-
-      @wares, @links = {}, { personal: [], inspired: [] }
 
       @settings[:markdown_options][:auto_ids] = false
+      @settings[:output][:posts] = 'interviews'
+      @settings[:archives] = {
+        years: '%Y',
+        months: '%B %Y',
+        days: false,
+        layout: 'interviews'
+      }
+
+      super
+
+      @output_paths[:wares] = File.join(@source_paths[:root], 'data', 'wares')
+      @output_paths[:links] = File.join(@source_paths[:root], 'data', 'links')
+
+      @wares, @links = {}, { personal: [], inspired: [] }
     end
 
     def scan_files
       super
 
-      Dir.glob(File.join(@paths[:wares], '**', '*.yml')).each do |path|
+      Dir.glob(File.join(@output_paths[:wares], '**', '*.yml')).each do |path|
         ware = UsesThis::Ware.new(path)
         @wares[ware.slug] = ware
       end
 
-      Dir.glob(File.join(@paths[:links], '**', '*.yml')).each do |path|
+      Dir.glob(File.join(@output_paths[:links], '**', '*.yml')).each do |path|
         link = UsesThis::Link.new(path)
         @links[link.summary ? :inspired : :personal] << link
       end
