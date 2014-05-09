71425d49b6b5fb63b32bd473a85d8ddd480c57ae
Daniel Bogan <d@waferbaby.com>
2014-05-08 21:58:56 -0700

    Reworked the markdown method to support optionally return the unrendered text. Added the to_hash and to_markdown methods.

--- lib/usesthis/interview.rb
+++ lib/usesthis/interview.rb
@@ -13,6 +13,11 @@ module UsesThis
 
       @hardware = {}
       @software = {}
+
+      @markdown = {
+        rendered: false,
+        raw: false,
+      }
     end
 
     def scan_links
@@ -29,12 +34,10 @@ module UsesThis
       end
     end
 
-    def markdown
-      if @markdown.nil?
-        site = UsesThis::Site.instance
-        output = @contents
-
+    def markdown(render = true)
+      if !@markdown[:raw]
         wares = @hardware.merge(@software)
+        output = @contents
 
         if wares.length > 0
           output += "\n\n"
@@ -44,14 +47,62 @@ module UsesThis
           end
         end
 
-        @markdown = site.markdown_renderer.render(output)
+        @markdown[:raw] = output
       end
 
-      @markdown
+      return @markdown[:raw] unless render
+
+      if !@markdown[:rendered]
+        site = UsesThis::Site.instance
+        output = @markdown[:raw]
+
+        @markdown[:rendered] = site.markdown_renderer.render(output)
+      end
+
+      @markdown[:rendered]
     end
 
     def output_path(site, parent_path)
       File.join(parent_path, @slug)
+    end
+
+    def to_hash
+      data = {
+        name: @name,
+        summary: @summary,
+        date: @date,
+        categories: @categories,
+        contents: @contents,
+      }
+
+      data[:wares] = {}
+
+      @hardware.merge(@software).each do |slug, ware|
+        data[:wares][slug] = ware.to_hash
+      end
+
+      data
+    end
+
+    def to_markdown
+      output = <<MARKDOWN
+# Interview
+
+![#{name}](http://usesthis.com/images/portraits/#{slug}.jpg)
+
+## #{name}
+#{summary} @ #{date}
+
+MARKDOWN
+
+      categories.each do |category|
+        output << "- #{category}\n"
+      end
+
+      output << "\n"
+      output << markdown(false)
+
+      output
     end
   end
 end
