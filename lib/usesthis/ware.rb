1045326af7a26955b28afd0a7171be9ed594d43b
Daniel Bogan <d@waferbaby.com>
2014-05-18 00:08:11 -0700

    Changed the class to be a subclass of the Page class, so we can generate pages from it.

--- lib/usesthis/ware.rb
+++ lib/usesthis/ware.rb
@@ -1,21 +1,32 @@
 module UsesThis
-  class Ware
+  class Ware < Salt::Page
     
+    HARDWARE = 'hardware'
+    SOFTWARE = 'software'
+
     attr_accessor :slug
     attr_accessor :name
     attr_accessor :description
     attr_accessor :url
+    attr_accessor :type
     attr_accessor :interviews
 
-    def initialize(path)
-      metadata = YAML::load_file(path)
-
+    def initialize(site, path, type)
+      super(site, path)
+      
       @slug = File.basename(path, File.extname(path))
 
-      @name = metadata['name']
-      @description = metadata['description']
-      @url = metadata['url']
-      @interviews = []
+      @layout = 'ware'
+      @filename = 'index'
+      
+      @title = @name
+      @type = type
+
+      @interviews = {}
+    end
+
+    def output_path(parent_path)
+      File.join(parent_path, @type, @slug)
     end
 
     def to_hash
