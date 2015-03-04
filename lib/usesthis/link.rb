bce9b5da35e97460e3116077989c64d6d10339cf
Daniel Bogan <d@waferbaby.com>
2015-03-03 19:16:04 -0800

    Initial import, again.

---
+++ lib/usesthis/link.rb
@@ -0,0 +1,18 @@
+module UsesThis
+  class Link
+    attr_accessor :slug
+    attr_accessor :name
+    attr_accessor :url
+    attr_accessor :description
+
+    def initialize(path)
+      metadata = YAML::load_file(path)
+
+      @slug = File.basename(path, File.extname(path))
+      
+      @name = metadata['name']
+      @url = metadata['url']
+      @description = metadata['description'] || ''
+    end
+  end
+end
