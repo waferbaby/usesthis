485e3dab828c56371d667a2e140d0ae3c81176b0
Daniel Bogan <d@waferbaby.com>
2015-03-03 19:16:26 -0800

    Required the link class.

--- lib/usesthis.rb
+++ lib/usesthis.rb
@@ -2,8 +2,9 @@ $:.unshift(__dir__)
 
 require 'dimples'
 
-require 'usesthis/interview'
-require 'usesthis/ware'
 require 'usesthis/site'
+require 'usesthis/interview'
+require 'usesthis/link'
+require 'usesthis/ware'
 
 Tilt.register Tilt::RedcarpetTemplate, 'interview'
