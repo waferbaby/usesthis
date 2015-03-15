09a261a1927faf4c1bc75fe7744fa352abec34c6
Daniel Bogan <d@waferbaby.com>
2015-03-15 14:00:23 -0700

    Set a path member that defines the base API path. Added the generate_interview_categories method.

--- lib/usesthis/api.rb
+++ lib/usesthis/api.rb
@@ -3,15 +3,18 @@ module UsesThis
     API_VERSION = "1"
 
     attr_accessor :site
+    attr_accessor :path
 
     def initialize(site)
       @site = site
+      @path = File.join(@site.output_paths[:site], 'api', "v#{API_VERSION}")
     end
 
     def generate
       prepare
 
       generate_interviews
+      generate_interview_categories
       generate_gear
     end
 
@@ -25,7 +28,7 @@ module UsesThis
 
       begin
         paths.each do |type|
-          FileUtils.mkdir_p(File.join(@site.output_paths[:site], 'api', "v#{API_VERSION}", type))
+          FileUtils.mkdir_p(File.join(@path, type))
         end
       rescue => e
         raise "Failed to prepare the API directories (#{e})"
@@ -34,7 +37,7 @@ module UsesThis
 
     def generate_interviews
       interviews = []
-      path = File.join(@site.output_paths[:site], 'api', "v#{API_VERSION}", 'interviews')
+      path = File.join(@path, 'interviews')
       
       @site.posts.each do |interview|
         interview_hash = interview.to_h
@@ -49,10 +52,34 @@ module UsesThis
       generate_json_file(path, 'index', {interviews: interviews})
     end
 
+    def generate_interview_categories
+      categories = []
+      path = File.join(@path, 'interviews', 'categories')
+
+      @site.categories.each_key do |category|
+        interviews = []
+
+        @site.categories[category].each do |interview|
+          interview_hash = interview.to_h
+
+          interview_hash.delete(:contents)
+          interview_hash.delete(:gear)
+
+          interviews << interview_hash
+        end
+
+        generate_json_file(path, category, {category: category, interviews: interviews})
+
+        categories << category
+      end
+
+      generate_json_file(path, 'index', {categories: categories.sort!})
+    end
+
     def generate_gear
       %w{hardware software}.each do |type|
         gear = []
-        path = File.join(@site.output_paths[:site], 'api', "v#{API_VERSION}", type)
+        path = File.join(@path, type)
 
         @site.send("#{type}").each do |slug, ware|
           ware_hash = ware.to_h
