fe3bef82a05b224bfc6bd6fcbbdfe12489e6439e
Daniel Bogan <d@waferbaby.com>
2016-07-04 20:51:13 -0700

    Reworked to incorporate the API directly.

--- lib/usesthis/site.rb
+++ lib/usesthis/site.rb
@@ -1,5 +1,6 @@
 module UsesThis
   class Site < Dimples::Site
+    API_VERSION = '1'.freeze
     REDIS_STATS_KEY = 'usesthis_stats'.freeze
 
     attr_accessor :hardware
@@ -22,9 +23,25 @@ module UsesThis
       @source_paths[:wares] = File.join(@source_paths[:root], 'data', 'gear')
       @source_paths[:links] = File.join(@source_paths[:root], 'data', 'links')
 
+      @output_paths[:api] = { root: File.join(@output_paths[:site], 'api', "v#{API_VERSION}") }
+
+      %w(interviews hardware software categories).each do |item|
+        path = File.join(@output_paths[:api][:root], item)
+        @output_paths[:api][item.to_sym] = path
+      end
+
       @stats = { hardware: { all: [], years: {} }, software: { all: [], years: {} } }
+      @api_data = { interviews: [], hardware: [], software: [], categories: [] }
 
       @post_class = UsesThis::Interview
+    end
+
+    def prepare_output_directory
+      super
+
+      @output_paths[:api].each_value do |path|
+        FileUtils.mkdir_p(path)
+      end
     end
 
     def scan_files
@@ -36,8 +53,6 @@ module UsesThis
       scan_links
 
       super
-
-      collect_stats
     end
 
     def scan_gear
@@ -57,31 +72,128 @@ module UsesThis
       end
     end
 
-    def prepare_post(post)
+    def generate
       super
 
-      %w(hardware software).each do |type|
-        post.send(type).each_value do |item|
-          send(type)[item.slug].interviews << post
+      generate_gear('hardware')
+      generate_gear('software')
 
-          keys = ["#{type}:all", "#{type}:#{post.date.year}"]
-
-          keys.each do |key|
-            redis_connection.sadd(REDIS_STATS_KEY, key)
-            redis_connection.zincrby(key, 1, item.slug)
-          end
-        end
-      end
+      generate_stats
     end
 
-    def collect_stats
+    def generate_posts
+      super
+
+      path = @output_paths[:api][:interviews]
+      generate_api_file(path, interviews: @api_data[:interviews])
+    end
+
+    def generate_post(interview)
+      super
+
+      interview_hash = interview.to_h
+
+      path = File.join(@output_paths[:api][:interviews], interview.slug)
+      generate_api_file(path, interview: interview_hash)
+
+      interview_hash.delete(:contents)
+      interview_hash.delete(:gear)
+
+      @api_data[:interviews] << interview_hash
+
+      record_interview_stats(interview)
+    end
+
+    def generate_categories
+      super
+
+      path = @output_paths[:api][:categories]
+      generate_api_file(path, categories: @api_data[:categories].sort!)
+    end
+
+    def generate_category(slug, category_interviews)
+      super
+
+      interviews = []
+
+      category_interviews.each do |interview|
+        interview_hash = interview.to_h
+
+        interview_hash.delete(:contents)
+        interview_hash.delete(:gear)
+
+        interviews << interview_hash
+      end
+
+      path = File.join(@output_paths[:api][:categories], slug)
+      generate_api_file(path, interviews: interviews)
+
+      @api_data[:categories] << slug
+    end
+
+    def generate_gear(type)
+      type_sym = type.to_sym
+      gear = []
+
+      send("#{type}").each_value do |ware|
+        ware_hash = ware.to_h
+
+        path = File.join(@output_paths[:api][type_sym], ware.slug)
+        generate_api_file(path, gear: ware_hash)
+
+        ware_hash.delete(:description)
+        ware_hash.delete(:url)
+        ware_hash.delete(:interviews)
+
+        gear << ware_hash
+      end
+
+      generate_api_file(@output_paths[:api][type_sym], gear: gear)
+    end
+
+    def generate_stats
       %w(hardware software).each do |type|
         type_sym = type.to_sym
 
         @stats[type_sym][:all] = fetch_stats_by_key("#{type}:all")
 
+        path = File.join(@output_paths[:api][type_sym], 'stats')
+
+        FileUtils.mkdir(path)
+        generate_api_file(path, gear: @stats[type_sym][:all])
+
         @archives[:year].keys.each do |year|
           @stats[type_sym][:years][year] = fetch_stats_by_key("#{type}:#{year}")
+
+          year_path = File.join(path, year)
+
+          FileUtils.mkdir(year_path)
+          generate_api_file(year_path, gear: @stats[type_sym][:years][year])
+        end
+      end
+    end
+
+    def generate_api_file(path, contents)
+      file = @page_class.new(self)
+
+      file.filename = 'index'
+      file.extension = 'json'
+      file.contents = Oj.dump(contents, indent: 2)
+
+      file.write(path, false)
+    end
+
+    def record_interview_stats(interview)
+      %w(hardware software).each do |type|
+        interview.send(type).each_value do |item|
+          send(type)[item.slug].interviews << interview
+
+          keys = ["#{type}:all", "#{type}:#{interview.date.year}"]
+
+          keys.each do |key|
+            redis_connection.sadd(REDIS_STATS_KEY, key)
+            redis_connection.zincrby(key, 1, item.slug)
+          end
         end
       end
     end
