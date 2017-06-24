cde36e3e62283775c06f7a403aeebf13b9bbc4f0
Daniel Bogan <d@waferbaby.com>
2017-06-24 21:16:00 +1000

    Split read_fixture into read_json_fixture and read_yaml_fixture.

--- test/helper.rb
+++ test/helper.rb
@@ -6,6 +6,7 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'usesthis'
 require 'json'
+require 'yaml'
 
 def test_site
   @test_site ||= UsesThis::Site.new(test_configuration)
@@ -27,15 +28,22 @@ def temp_site_path
 end
 
 def test_interview
-  @test_interview ||= read_fixture('interview')
+  @test_interview ||= read_json_fixture('interview')
+end
+
+def fixture_path(name)
+  File.join(__dir__, 'fixtures', name)
+end
+
+def read_json_fixture(name)
+  JSON.parse(File.read(fixture_path("#{name}.json")))
+end
+
+def read_yaml_fixture(name)
+  YAML.load_file(fixture_path("#{name}.yml"))
 end
 
 def read_api_file(path = nil)
   path = File.join(test_site.output_paths[:site], 'api', path, 'index.json')
-  JSON.parse(File.read(path))
-end
-
-def read_fixture(name)
-  path = File.join(__dir__, 'fixtures', "#{name}.json")
   JSON.parse(File.read(path))
 end
