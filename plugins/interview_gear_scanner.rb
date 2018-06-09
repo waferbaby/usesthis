78f6af8816ad09815362d268c4d71fb6806e93fa
Daniel Bogan <d@waferbaby.com>
2018-06-09 20:00:48 +1000

    Make Rubocop happy

--- plugins/interview_gear_scanner.rb
+++ plugins/interview_gear_scanner.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 require 'json'
 
+# A class for pulling in Markdown gear links via JSON files.
 class InterviewGearScanner < Dimples::Plugin
   GEAR_LINK = /\[([^\[\(\)]+)\]\[([a-z0-9\.\-]+)?\]/
 
   def gear
     @gear ||= {}.tap do |gear|
-      path = File.join(@site.paths[:base], 'gear', '**', '*.json')
+      glob_path = File.join(@site.paths[:source], 'gear', '**', '*.json')
 
-      Dir.glob(path).sort.each do |path|
+      Dir.glob(glob_path).sort.each do |path|
         slug = File.basename(path, '.json')
-        gear[slug] = JSON.load(File.read(path))
+        gear[slug] = JSON.parse(File.read(path))
       end
     end
   end
@@ -21,7 +24,7 @@ class InterviewGearScanner < Dimples::Plugin
   def process(event, interview)
     case event
     when :before_post_write
-      links = ''
+      links = []
 
       interview.contents.scan(GEAR_LINK).each do |link|
         slug = (link[1] ? link[1] : link[0].downcase)
@@ -30,7 +33,7 @@ class InterviewGearScanner < Dimples::Plugin
         end
       end
 
-      interview.contents << "\n\n#{links}" unless links.empty?
+      interview.contents << "\n\n#{links.join}" unless links.empty?
     end
   end
 end
