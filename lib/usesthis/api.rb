5e2e54582c8941818b494d9870628f802f189908
Daniel Bogan <d@waferbaby.com>
2019-06-25 22:32:54 +1000

    Refactor the code, add category endpoints

--- lib/usesthis/api.rb
+++ lib/usesthis/api.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'date'
 require 'dimples'
 require 'json'
+require 'time'
 
 module UsesThis
   # A class modelling the usesthis.com API.
@@ -10,29 +10,27 @@ module UsesThis
     VERSION = 2
     URL = "https://usesthis.com/api/v#{VERSION}"
     LINK_PATTERN = /^\[(.+)\]: http.+/.freeze
-    INTERVIEW_FILENAME_PATTERN = /(\d{4}-\d{2}-\d{2})-(.+)/.freeze
-    SLICE_KEYS = %i[slug name title description summary url api_url].freeze
+    DATE_SLUG_PATTERN = /(\d{4}-\d{2}-\d{2})-(.+)/.freeze
+    SLICE_KEYS = %i[slug name title description date summary url api_url].freeze
     PAGER_OPTIONS = { page_prefix: '?page=', per_page: 50 }.freeze
 
-    attr_accessor :paths, :gear, :interviews, :stats
+    attr_accessor :paths, :gear, :interviews, :categories, :stats
 
-    def self.generate(source_path, destination_path)
-      new(source_path, destination_path).generate
+    def self.generate(source_path, destination_path, options = {})
+      new(source_path, destination_path, options).generate
     end
 
-    def initialize(source_path, destination_path)
+    def initialize(source_path, destination_path, options = {})
       @paths = {
         source: source_path,
         destination: File.join(destination_path, 'api', "v#{VERSION}")
       }
 
-      %i[interviews hardware software stats].each do |section|
-        @paths[section] = File.join(@paths[:destination], section.to_s)
-      end
-
       @gear = {}
       @interviews = {}
+      @categories = {}
       @stats = {}
+      @options = options
     end
 
     def generate
@@ -41,6 +39,7 @@ module UsesThis
 
       generate_gear_endpoints
       generate_interview_endpoints
+      generate_category_endpoints
       generate_stat_endpoints
     end
 
@@ -77,6 +76,8 @@ module UsesThis
 
     def generate_gear_endpoints
       %i[hardware software].each do |type|
+        gear_path = File.join(@paths[:destination], type.to_s)
+
         pager = Dimples::Pager.new(
           "#{URL}/#{type}/",
           @gear[type].to_a,
@@ -90,59 +91,67 @@ module UsesThis
           end
 
           output = { gear: paged_gear, links: pager.to_context[:urls] }
-          generate_paged_json_file(@paths[type], index, output)
+          generate_paged_json_file(gear_path, index, output)
         end
 
         @gear[type].each do |slug, item|
-          path = File.join(@paths[type].to_s, slug.to_s, 'index.json')
+          path = File.join(gear_path, slug.to_s, 'index.json')
           generate_json_file(path, item: item)
         end
       end
     end
 
     def generate_interview_endpoints
-      pager = Dimples::Pager.new(
+      interviews_path = File.join(@paths[:destination], 'interviews')
+
+      generate_paginated_interview_endpoints(
+        interviews_path,
         "#{URL}/interviews/",
-        @interviews.to_a,
-        PAGER_OPTIONS
+        @interviews.values
       )
 
-      pager.each do |index|
-        paged_interviews = pager.posts_at(index).map do |_, interview|
-          interview.slice(*SLICE_KEYS)
-        end
-
-        output = {
-          interviews: paged_interviews,
-          links: pager.to_context[:urls]
-        }
-
-        generate_paged_json_file(@paths[:interviews], index, output)
-      end
-
       @interviews.each do |slug, interview|
-        path = File.join(@paths[:interviews], slug.to_s, 'index.json')
+        path = File.join(interviews_path, slug.to_s, 'index.json')
         generate_json_file(path, interview: interview)
       end
     end
 
+    def generate_category_endpoints
+      categories_path = File.join(@paths[:destination], 'categories')
+
+      categories = @categories.keys.sort.map do |slug|
+        @categories[slug].slice(*SLICE_KEYS)
+      end
+
+      path = File.join(categories_path, 'index.json')
+      generate_json_file(path, categories: categories)
+
+      @categories.each do |slug, category|
+        category_path = File.join(categories_path, slug)
+
+        generate_paginated_interview_endpoints(
+          category_path,
+          "#{URL}/categories/#{slug}",
+          category[:interviews]
+        )
+      end
+    end
+
     def generate_stat_endpoints
+      stats_path = File.join(@paths[:destination], 'stats')
+
       stats = {
         interviews: @interviews.count,
         hardware: @gear[:hardware].count,
         software: @gear[:software].count
       }
 
-      generate_json_file(File.join(@paths[:stats], 'index.json'), stats: stats)
+      generate_json_file(File.join(stats_path, 'index.json'), stats: stats)
 
       %i[hardware software].each do |type|
         @stats[type].each do |key, values|
-          path = File.join(
-            @paths[:stats],
-            type.to_s,
-            key != :all ? key.to_s : '',
-            'index.json'
-          )
+          time_period = key != :all ? key.to_s : ''
+          path = File.join(stats_path, type.to_s, time_period, 'index.json')
 
           output = values.sort_by(&:last).reverse[0..49].map do |slug, count|
             @gear[type][slug.to_sym].slice(*SLICE_KEYS).merge(count: count)
@@ -165,34 +174,48 @@ module UsesThis
       )
     end
 
-    def prepare_interview(path)
-      base_name = File.basename(path, '.markdown')
-      date_string, slug = base_name.scan(INTERVIEW_FILENAME_PATTERN)[0]
+    def generate_paginated_interview_endpoints(path, url, interviews)
+      pager = Dimples::Pager.new(url, interviews, PAGER_OPTIONS)
 
-      data = File.read(path)
-      date = Date.parse(date_string)
+      pager.each do |index|
+        paged_interviews = pager.posts_at(index).map do |interview|
+          interview.slice(*SLICE_KEYS)
+        end
 
-      contents, interview = Dimples::FrontMatter.parse(data)
-      interview[:name] = interview.delete(:title)
+        output = {
+          interviews: paged_interviews,
+          links: pager.to_context[:urls]
+        }
 
-      interview.merge!(
-        slug: slug,
-        url: "https://usesthis.com/interviews/#{slug}",
-        api_url: "#{URL}/interviews/#{slug}",
-        contents: contents,
-        date: date.iso8601
-      )
-
-      interview[:gear] = contents.scan(LINK_PATTERN).flatten.map do |gear_slug|
-        item = find_gear(gear_slug)
-        item[:interviews] << interview.slice(*SLICE_KEYS)
-
-        increment_gear_count(gear_slug, date.year)
-
-        item.slice(*SLICE_KEYS)
+        generate_paged_json_file(path, index, output)
       end
+    end
 
-      interview
+    def prepare_interview(path)
+      year, month, day, slug = File.basename(path, '.markdown').split('-')
+
+      contents, output = Dimples::FrontMatter.parse(File.read(path))
+
+      output.tap do |interview|
+        interview.merge!(
+          slug: slug,
+          name: interview.delete(:title),
+          url: "https://usesthis.com/interviews/#{slug}",
+          api_url: "#{URL}/interviews/#{slug}",
+          contents: contents,
+          date: Time.new(year, month, day).iso8601,
+          gear: []
+        )
+
+        interview[:categories].each do |category_slug|
+          add_category_interview(category_slug, interview)
+        end
+
+        contents.scan(LINK_PATTERN).flatten.map do |gear_slug|
+          add_gear_interview(gear_slug, interview)
+          interview[:gear] << find_gear(gear_slug).slice(*SLICE_KEYS)
+        end
+      end
     end
 
     def generate_json_file(path, data)
@@ -209,6 +232,29 @@ module UsesThis
                  end
 
       generate_json_file(File.join(path, filename), data)
+    end
+
+    def add_category_interview(category_slug, interview)
+      name = @options[:category_names] &&
+             @options[:category_names][category_slug] ||
+             category_slug.capitalize
+
+      @categories[category_slug] ||= {
+        slug: category_slug,
+        name: name,
+        url: "http://usesthis.com/categories/#{category_slug}",
+        api_url: "#{URL}/categories/#{category_slug}",
+        interviews: []
+      }
+
+      @categories[category_slug][:interviews] << interview.slice(*SLICE_KEYS)
+    end
+
+    def add_gear_interview(gear_slug, interview)
+      item = find_gear(gear_slug)
+      item[:interviews] << interview.slice(*SLICE_KEYS)
+
+      increment_gear_count(gear_slug, interview[:date][0..3])
     end
 
     def increment_gear_count(slug, year)
